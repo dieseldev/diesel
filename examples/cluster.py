d2bd1eb5ba96b6394e2ea83f1151becd47c647e4
Jamie Turner <jamwt@boomplex.com>
2009-09-10 11:04:24 +0000

    Cluster example client.

---
+++ examples/cluster.py
@@ -0,0 +1,21 @@
+import sys
+from concussion import Cluster, Application, Loop
+from concussion.logmod import Logger, LOGLVL_DEBUG
+
+this_port = {
+	'1' : 10001,
+	'2' : 10002,
+	'3' : 10003,
+}[sys.argv[1]]
+	
+a = Application(
+logger=Logger(verbosity=LOGLVL_DEBUG),
+cluster=Cluster([('wimpy', 10001), ('wimpy', 10002), ('wimpy', 10003)], port=this_port), )
+
+def reg_loop():
+	for x in xrange(0, 100):
+		yield a.cluster.register("foobar%s" % x, None)
+
+a.add_loop(Loop(reg_loop))
+
+a.run()
