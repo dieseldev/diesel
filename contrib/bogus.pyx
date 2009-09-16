7f9b33f0295946a5ecb9388798590a6c4f93719d
Jamie Turner <jamwt@boomplex.com>
2009-09-16 07:16:59 +0000

    More setup niceties.

---
+++ contrib/bogus.pyx
@@ -0,0 +1,2 @@
+# No real content here.  Just used to make sure
+# the lexicon is generated
--- setup.py
+++ setup.py
@@ -10,9 +10,10 @@ except ImportError:
  REQUIRED SOFTWARE IS MISSING!
 ------------------------------------------------------------------------
 
-diesel requires pyevent and libevent.  
+diesel requires pyevent and libevent >= 1.4.  
 
-libevent can most likely be found in your apt/yum repository:
+libevent can most likely be found in your apt/yum repository (make sure
+it is at least version 1.4):
 
  sudo apt-get install libevent
 
@@ -25,21 +26,29 @@ and install it.  Then try again!
 
 If you _do_ have libevent, we recommend you allow us to install 
 pyevent--it's packaged with diesel.
+
+To install pyevent, you'll need Pyrex:
+
+ sudo easy_install -UZ Pyrex
 """
 
 	if raw_input('Install pyevent (y/N)? ').strip().lower() == 'y':
+		pyx_prepare = 'pyrexc -o /dev/null contrib/bogus.pyx'
 		cmd = 'easy_install -UZ contrib/event-0.4-diesel.tar.gz'
 		if raw_input('Use sudo (y/N)? ').strip().lower() == 'y':
+			os.system('sudo ' + pyx_prepare)
 			os.system('sudo ' + cmd)
 		else:
+			os.system(pyx_prepare)
 			os.system(cmd)
 		try:
 			import event
 		except ImportError:
-			print "That didn't work.. I give up!"
+			print "pyevent still not working.. aborting!"
 			raise SystemExit(2)
 		else:
 			print "pyevent ... [OK]"
+
 	else:
 		print 'Quitting diesel installation...'
 		raise SystemExit(1)
@@ -48,6 +57,6 @@ else:
 
 setup(name="diesel",
 	version="0.9.0b",
-	author="Jamie Turner/Boomplex LLC",
+	author="Boomplex LLC",
 	packages=["diesel", "diesel.protocols"],
 	)
