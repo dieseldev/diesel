dc0c362ef266ab1300d5e8fa29545654d7fe8c41
Jamie Turner <jamwt@boomplex.com>
2009-09-17 01:44:01 -0700

    setup bump

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import sys
 assert sys.version_info >= (2, 6), \
 "Diesel requires python 2.6 (or greater 2.X release)"
+import select
+assert hasattr(select, 'EPOLLIN'), \
+"Diesel requires a linux system with epoll (2.6+ kernel)"
 
 from setuptools import setup
 
