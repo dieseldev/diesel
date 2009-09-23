00b2669099dca1b5784de7384fe2132e3a5253bc
Jamie Turner <jamwt@boomplex.com>
2009-09-23 06:55:27 +0000

    Setup updated.

--- setup.py
+++ setup.py
@@ -7,8 +7,24 @@ assert hasattr(select, 'EPOLLIN'), \
 
 from setuptools import setup
 
+VERSION = "0.9.0b"
+
 setup(name="diesel",
-	version="0.9.0b",
+	version=VERSION,
 	author="Boomplex LLC",
+	author_email="dev@boomplex.com",
+	description="Diesel is a generator-based asynchronous I/O library for Python",
+	long_description='''
+diesel is a framework for writing network applications using asynchronous 
+I/O in Python.
+
+It uses Python's generators to provide a friendly syntax for coroutines 
+and continuations. It performs well and handles high concurrency with ease.
+
+An HTTP/1.1 implementation is included as an example, which can be used 
+for building web applications.
+''',
+	url="http://dieselweb.org",
+	download_url="http://download.dieselweb.org/diesel-%s.tar.gz" % VERSION, 
 	packages=["diesel", "diesel.protocols"],
 	)
