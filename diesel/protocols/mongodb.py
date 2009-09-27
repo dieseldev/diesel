82c868619c2536f493d789896e74c5b36e573d44
Christian Wyglendowski <christian@dowski.com>
2009-09-26 20:27:22 -0400

    Initial checkin of mongodb client.

---
+++ diesel/protocols/mongodb.py
@@ -0,0 +1,354 @@
+"""A mongodb client library for Diesel"""
+
+import itertools
+import struct
+from diesel import Client, call, response, bytes, Loop, Application, up
+from pymongo.bson import BSON, _make_c_string, _to_dicts
+from pymongo.son import SON
+
+_ZERO = "\x00\x00\x00\x00"
+
+
+def _full_name(parent, child):
+    return "%s.%s" % (parent, child)
+
+class TraversesCollections(object):
+    def __init__(self, name, client):
+        self.name = name
+        self.client = client
+
+    def __getattr__(self, name):
+        return Collection(_full_name(self.name, name), self.client)
+
+class Db(TraversesCollections):
+    pass
+
+class Collection(TraversesCollections):
+    def find(self, spec=None, fields=None, skip=0, limit=0):
+        yield up(MongoCursor(self.name, self.client, spec, fields, skip, limit))
+
+    def update(self, spec, doc, upsert=0):
+        yield self.client.update(self.name, spec, doc, upsert)
+
+    def insert(self, doc_or_docs):
+        yield self.client.insert(self.name, doc_or_docs)
+
+    def delete(self, spec):
+        yield self.client.delete(self.name, spec)
+
+class MongoClient(Client):
+    def __init__(self, *args, **params):
+        Client.__init__(self, *args, **params)
+        self._msg_id_counter = itertools.count(1)
+
+    @property
+    def _msg_id(self):
+        return self._msg_id_counter.next()
+
+    def _put_request_get_response(self, op, data):
+        yield self._put_request(op, data)
+        header = yield bytes(16)
+        length, id, to, code = struct.unpack('<4i', header)
+        to_read = length - 16
+        complete = 0
+        parts = []
+        while True:
+            # read in chunks so we give other coroutines a chance to run
+            part = yield bytes(512 if (to_read - complete) >= 512 else (to_read - complete))
+            parts.append(part)
+            complete += len(part)
+            if to_read == complete:
+                break
+        message = "".join(parts)
+        cutoff = struct.calcsize('<iqii')
+        flag, cid, start, numret = struct.unpack('<iqii', message[:cutoff])
+        body = _to_dicts(message[cutoff:])
+        yield up((cid, start, numret, body))
+
+    def _put_request(self, op, data):
+        req = struct.pack('<4i', 16 + len(data), self._msg_id, 0, op)
+        yield "%s%s" % (req, data)
+
+    def _handle_response(self, cursor, resp):
+        cid, start, numret, result = resp
+        cursor.retrieved += numret
+        cursor.id = cid
+        if not cid or (cursor.retrieved == cursor.limit):
+            cursor.finished = True
+        yield response(result)
+
+    @call
+    def query(self, cursor): #col, spec=None, fields=None, skip=0, limit=0):
+        op = Ops.OP_QUERY
+        c = cursor
+        msg = Ops.query(c.col, c.spec, c.fields, c.skip, c.limit)
+        resp = yield self._put_request_get_response(op, msg)
+        yield self._handle_response(cursor, resp)
+
+    @call
+    def get_more(self, cursor):
+        limit = 0
+        if cursor.limit:
+            if cursor.limit > cursor.retrieved:
+                limit = cursor.limit - cursor.retrieved
+            else:
+                cursor.finished = True
+        if not cursor.finished:
+            op = Ops.OP_GET_MORE
+            msg = Ops.get_more(cursor.col, limit, cursor.id)
+            resp = yield self._put_request_get_response(op, msg)
+            yield self._handle_response(cursor, resp)
+        else:
+            yield response([])
+
+    @call
+    def update(self, col, spec, doc, upsert=0):
+        data = Ops.update(col, spec, doc, upsert)
+        yield self._put_request(Ops.OP_UPDATE, data)
+        yield response(None)
+
+    @call
+    def insert(self, col, doc_or_docs):
+        data = Ops.insert(col, doc_or_docs)
+        yield self._put_request(Ops.OP_INSERT, data)
+        yield response(None)
+
+    @call
+    def delete(self, col, spec):
+        data = Ops.delete(col, spec)
+        yield self._put_request(Ops.OP_DELETE, data)
+        yield response(None)
+
+    @call
+    def drop_database(self, name):
+        yield response((yield self._command(name, {'dropDatabase':1})))
+
+    @call
+    def list_databases(self):
+        result = yield self._command('admin', {'listDatabases':1})
+        yield response([(d['name'], d['sizeOnDisk']) for d in result['databases']])
+
+    @call
+    def _command(self, dbname, command):
+        msg = Ops.query('%s.$cmd' % dbname, command, None, 0, 1)
+        resp = yield self._put_request_get_response(Ops.OP_QUERY, msg)
+        cid, start, numret, result = resp
+        if result:
+            yield response(result[0])
+        else:
+            yield response([])
+
+    def __getattr__(self, name):
+        return Db(name, self)
+
+class Ops(object):
+    ASCENDING = 1
+    DESCENDING = -1
+    OP_UPDATE = 2001
+    OP_INSERT = 2002
+    OP_GET_BY_OID = 2003
+    OP_QUERY = 2004
+    OP_GET_MORE = 2005
+    OP_DELETE = 2006
+    OP_KILL_CURSORS = 2007
+
+    @staticmethod
+    def query(col, spec, fields, skip, limit):
+        data = [
+            _ZERO, 
+            _make_c_string(col), 
+            struct.pack('<ii', skip, limit),
+            BSON.from_dict(spec or {}),
+        ]
+        if fields:
+            data.append(BSON.from_dict(dict.fromkeys(fields, 1)))
+        return "".join(data)
+
+    @staticmethod
+    def get_more(col, limit, id):
+        data = _ZERO
+        data += _make_c_string(col)
+        data += struct.pack('<iq', limit, id)
+        return data
+
+    @staticmethod
+    def update(col, spec, doc, upsert):
+        colname = _make_c_string(col)
+        fmt = '<i%dsi' % len(colname)
+        part = struct.pack(fmt, 0, colname, upsert)
+        return "%s%s%s" % (part, BSON.from_dict(spec), BSON.from_dict(doc))
+
+    @staticmethod
+    def insert(col, doc_or_docs):
+        try:
+            doc_or_docs.fromkeys
+            doc_or_docs = [doc_or_docs]
+        except AttributeError:
+            pass
+        doc_data = "".join(BSON.from_dict(doc) for doc in doc_or_docs)
+        colname = _make_c_string(col)
+        return "%s%s%s" % (_ZERO, colname, doc_data)
+
+    @staticmethod
+    def delete(col, spec):
+        colname = _make_c_string(col)
+        return "%s%s%s%s" % (_ZERO, colname, _ZERO, BSON.from_dict(spec))
+
+class MongoCursor(object):
+    def __init__(self, col, client, spec, fields, skip, limit):
+        self.col = col
+        self.client = client
+        self.spec = spec
+        self.fields = fields
+        self.skip = skip
+        self.limit = limit
+        self.id = None
+        self.retrieved = 0
+        self.finished = False
+        self._query_additions = []
+
+    def more(self):
+        if not self.retrieved:
+            self._touch_query()
+        if not self.id and not self.finished:
+            yield self.client.query(self)
+        elif not self.finished:
+            yield self.client.get_more(self)
+
+    def count(self):
+        if self.retrieved:
+            raise ValueError("can't count an already started cursor")
+        db, col = self.col.split('.', 1)
+        command = SON([('count', col), ('query', self.spec)])
+        result = yield self.client._command(db, command)
+        yield up(int(result.get('n', 0)))
+
+    def sort(self, name, direction):
+        if self.retrieved:
+            raise ValueError("can't sort an already started cursor")
+        key = SON()
+        key[name] = direction
+        self._query_additions.append(('sort', key))
+        return self
+
+    def _touch_query(self):
+        if self._query_additions:
+            spec = SON({'query': self.spec})
+            for k, v in self._query_additions:
+                if k == 'sort':
+                    ordering = spec.setdefault('orderby', SON())
+                    ordering.update(v)
+            self.spec = spec
+        
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **params):
+        if self.id and not self.finished:
+            raise RuntimeError("need to cleanup cursor!")
+
+if __name__ == '__main__':
+    from pprint import pprint
+    from diesel import fire, wait
+    HOST = 'localhost'
+    PORT = 27017
+    a = Application()
+
+    def mgr():
+        (main, queries) = yield (wait('main.done'), wait('queries.done'))
+        (main, queries) = yield (wait('main.done'), wait('queries.done'))
+        a.halt()
+
+    def query_20_times():
+        d = MongoClient()
+        d.connect(HOST, PORT)
+        counts = []
+        for i in range(20):
+            with (yield d.diesel.test.find({'type':'test'}, limit=500)) as cursor:
+                while not cursor.finished:
+                    counts.append(len((yield cursor.more())))
+        assert 0 in counts, counts
+        assert 500 in counts, counts
+        print "20 concurrent queries - passed"
+        yield fire('queries.done', True)
+
+    def pure_db_action():
+        d = MongoClient()
+        d.connect(HOST, PORT)
+        print (yield d.list_databases())
+        print (yield d.drop_database('diesel'))
+        yield d.diesel.test.insert({'name':'dowski', 'state':'OH'})
+        yield d.diesel.test.insert({'name':'jamwt', 'state':'CA'})
+        yield d.diesel.test.insert({'name':'mrshoe', 'state':'CA'})
+        with (yield d.diesel.test.find({'state':'OH'})) as cursor:
+            while not cursor.finished:
+                res = yield cursor.more()
+                assert res[0]['name'] == 'dowski', res
+                assert res[0]['state'] == 'OH', res
+                print "query1 (simple where) passed"
+        with (yield d.diesel.test.find({'state':'CA'})) as cursor:
+            while not cursor.finished:
+                res = yield cursor.more()
+                assert len(res) == 2, res
+                assert res[0]['name'] == 'jamwt', res
+                assert res[1]['name'] == 'mrshoe', res
+                print "query2 (simple where) passed"
+        with (yield d.diesel.test.find()) as cursor:
+            while not cursor.finished:
+                res = yield cursor.more()
+                assert len(res) == 3, res
+                assert [r['state'] for r in res] == ['OH', 'CA', 'CA'], res
+                print "query3 (query all) passed"
+        print "updating"
+        yield d.diesel.test.update({'name':'dowski'}, {'$set':{'kids':2}})
+        with (yield d.diesel.test.find()) as cursor:
+            while not cursor.finished:
+                res = yield cursor.more()
+                assert [r['kids'] for r in res if 'kids' in r] == [2], res
+                print "query4 (verify update) passed"
+        print "inserting"
+        yield d.diesel.test.insert({'name':'mr t', 'state':'??'})
+        with (yield d.diesel.test.find({'name':'mr t'}, ['state'])) as cursor:
+            while not cursor.finished:
+                res = yield cursor.more()
+                assert len(res) == 1, res
+                assert 'name' not in res[0], res
+                assert res[0]['state'] == '??', res
+                print "query5 (verify insert) passed"
+        print "deleting"
+        yield d.diesel.test.delete({'name':'mr t'})
+        with (yield d.diesel.test.find({'name':'mr t'}, ['state'])) as cursor:
+            while not cursor.finished:
+                res = yield cursor.more()
+                assert res == [], res
+                print "query6 (verify delete) passed"
+        print "inserting 10000"
+        yield d.diesel.test.insert([{'code':i, 'type':'test'} for i in xrange(10000)])
+        count = 0
+        passes = 0
+        with (yield d.diesel.test.find({'type':'test'})) as cursor:
+            while not cursor.finished:
+                count += len((yield cursor.more()))
+                passes += 1
+        assert count == 10000, count
+        assert passes == 2, passes
+        print "query7 (get_more) passed"
+        print "inserting"
+        yield (d.diesel.test.insert([{'letter':'m'}, {'letter':'b'}, {'letter':'k'}]))
+        with (yield d.diesel.test.find({'letter': {'$exists':True}})) as cursor:
+            cursor.sort('letter', Ops.DESCENDING)
+            while not cursor.finished:
+                res = yield cursor.more()
+                assert len(res) == 3, res
+                assert [r['letter'] for r in res] == ['m', 'k', 'b'], res
+                print "query8 (sorting) passed"
+        with (yield d.diesel.test.find({'type':'test'})) as cursor:
+            n = yield cursor.count()
+            assert n == 10000, n
+            print "query9 (count) passed"
+        yield fire('main.done', True)
+
+    a.add_loop(Loop(mgr))
+    a.add_loop(Loop(pure_db_action))
+    a.add_loop(Loop(query_20_times))
+    a.run()
