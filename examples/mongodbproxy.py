ba4b8f23e1d07859e2a9bbb973c84d7cf9242596
Christian Wyglendowski <dowski@boomplex.com>
2009-09-29 23:08:27 -0400

    Built out the mongodb proxy example a bit more so it is closer to a true pub/sub system.

--- examples/mongodbproxy.py
+++ examples/mongodbproxy.py
@@ -1,3 +1,4 @@
+import collections
 import struct
 from pymongo.bson import _make_c_string, BSON, _bson_to_dict
 from diesel import wait, fire, call, response, up, bytes
@@ -18,26 +19,63 @@ class SubscribingClient(MongoClient):
         msg = "".join(data)
         yield self._put_request(op, msg)
         doclen = struct.unpack('<i', (yield bytes(4)))[0]
-        doc = yield bytes(doclen)
-        yield response(BSON(doc).to_dict())
+        rawdoc = yield bytes(doclen)
+        objs = []
+        while rawdoc:
+            obj, rawdoc = _bson_to_dict(rawdoc)
+            objs.append(obj)
+        yield response(objs)
+
+class Channel(object):
+    """A channel that is useful for publishing and retrieving events."""
+    def __init__(self):
+        self.subscriptions = collections.defaultdict(list)
+
+    def get(self, subscriber):
+        """Remove and return any pending events for subscriber."""
+        waiting = self.subscriptions[subscriber][:]
+        self.subscriptions[subscriber][:] = []
+        return "".join(waiting)
+
+    def update(self, event):
+        """Publish a new event for each subscriber on the channel."""
+        for skey in self.subscriptions:
+            self.subscriptions[skey].append(event)
+
+    def __repr__(self):
+        return repr(self.subscriptions)
 
 class SubscriptionProxy(MongoProxy):
+    """
+     1) A client can connect, identify itself and a collection+document that it
+     wants to subscribe to.
+     2) Events on a collection+document are recorded here in the proxy.
+     3) If events occur while the client is away, they are notified of them
+     immediately the next time they return.
+    """
+    channels = collections.defaultdict(Channel)
+
     def handle_request(self, info, body):
         length, id, to, opcode = info
         if opcode in [OP_SUBSCRIBE, Ops.OP_UPDATE]:
             trimmed = body[4:]
             col, rest = trimmed.split('\0', 1)
             if opcode == OP_SUBSCRIBE:
+                subscriber, col = col.split('@')
                 sl, raw_bson = rest[:8], rest[8:]
                 spec = BSON(raw_bson).to_dict()
-                doc = yield wait(('update', col, str(spec)))
-                doclen = len(doc)
-                resp = "%s%s" % (struct.pack('<i', doclen), doc)
+                waiting = self.channels[(col, str(spec))].get(subscriber)
+                if not waiting:
+                    yield wait(('update', col, str(spec)))
+                    waiting = self.channels[(col, str(spec))].get(subscriber)
+                wlen = len(waiting)
+                resp = "%s%s" % (struct.pack('<i', wlen), waiting)
                 yield up((resp, info, body))
             elif opcode == Ops.OP_UPDATE:
                 upsert, raw_bson = rest[:4], rest[4:]
                 spec, raw_doc = _bson_to_dict(raw_bson)
-                yield fire(('update', col, str(spec)), raw_doc)
+                uid = self.channels[(col, str(spec))].update(raw_doc)
+                yield fire(('update', col, str(spec)))
                 yield up((None, info, body))
         else:
             yield up((None, info, body))
@@ -47,29 +85,55 @@ if __name__ == '__main__':
     BACKEND_HOST = 'localhost'
     BACKEND_PORT = 27017
     FRONTEND_PORT = 27018
-    def subber():
-        c = SubscribingClient()
-        c.connect(BACKEND_HOST, FRONTEND_PORT)
-        print "subber: subscribing ..."
-        doc = yield c.subscribe('sub.test', {'room':'general'})
-        print "subber: saw updated doc %r" % doc
-        with (yield c.sub.test.find({'room':'general'})) as cursor:
-            result = yield cursor.more()
-            print "subber: got", result
-        a.halt()
 
-    def pubber():
+    def main():
         c = MongoClient()
         c.connect(BACKEND_HOST, FRONTEND_PORT)
-        print "pubber: sleeping ..."
+        yield c.drop_database('sub')
+        print "main: dropped the db"
+        a.add_loop(Loop(subscriber))
+        a.add_loop(Loop(publisher))
+        print "main: loops started"
+        c.close()
+
+    def subscriber():
+        c = SubscribingClient()
+        c.connect(BACKEND_HOST, FRONTEND_PORT)
+        print "subscriber: subscribing ..."
+        events = yield c.subscribe('subscriber@sub.test', {'room':'general'})
+        print "subscriber: saw events %r" % events
+        with (yield c.sub.test.find({'room':'general'})) as cursor:
+            result = yield cursor.more()
+            print "subscriber: initial room state", result
+        print "subscriber: sleeping for 10 ..."
+        yield sleep(10)
+        print "subscriber: woke up"
+        events = yield c.subscribe('subscriber@sub.test', {'room':'general'})
+        assert len(events) == 2
+        print "subscriber: there were %d events while i was sleeping" % len(events)
+        print "subscriber: events: %r" % events
+        with (yield c.sub.test.find({'room':'general'})) as cursor:
+            result = yield cursor.more()
+            print "subscriber: final room state", result
+        a.halt()
+
+    def publisher():
+        c = MongoClient()
+        c.connect(BACKEND_HOST, FRONTEND_PORT)
+        print "publisher: sleeping ..."
         yield sleep(5)
-        print "pubber: updating ..."
-        yield c.sub.test.update({'room':'general'}, {'$set': {'users':['pubber']}}, upsert=1)
-        print "pubber: updated"
+        print "publisher: updating ..."
+        yield c.sub.test.update({'room':'general'}, {'$push': {'users':'publisher'}}, upsert=1)
+        print "publisher: updated"
+        yield sleep(2)
+        yield c.sub.test.update({'room':'general'}, {'$push': {'users':'moe'}}, upsert=1)
+        print "publisher: updated the doc"
+        yield sleep(2)
+        yield c.sub.test.update({'room':'general'}, {'$pull': {'users':'publisher'}}, upsert=1)
+        print "publisher: updated the doc"
 
     a = Application()
     a.add_service(Service(SubscriptionProxy(BACKEND_HOST, BACKEND_PORT), FRONTEND_PORT))
-    a.add_loop(Loop(subber))
-    a.add_loop(Loop(pubber))
+    a.add_loop(Loop(main))
     a.run()
 
