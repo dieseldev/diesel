b2f40247682a0132d7c4156a6787c359682a014a
Christian Wyglendowski <dowski@boomplex.com>
2009-09-30 23:15:08 -0400

    More pub/sub improvements to the mongodbproxy example.

--- examples/mongodbproxy.py
+++ examples/mongodbproxy.py
@@ -5,11 +5,12 @@ from diesel import wait, fire, call, response, up, bytes
 from diesel.protocols.mongodb import MongoProxy, Ops, MongoClient
 
 OP_SUBSCRIBE = 1500
+OP_WAIT = 1501
+PROXIED_OPS = set([OP_WAIT, OP_SUBSCRIBE, Ops.OP_UPDATE])
 
 class SubscribingClient(MongoClient):
     @call
     def subscribe(self, col, spec):
-        op = OP_SUBSCRIBE
         data = [
             "\x00\x00\x00\x00",
             _make_c_string(col), 
@@ -17,7 +18,13 @@ class SubscribingClient(MongoClient):
             BSON.from_dict(spec),
         ]
         msg = "".join(data)
-        yield self._put_request(op, msg)
+        yield self._put_request(OP_SUBSCRIBE, msg)
+        yield response('')
+
+    @call
+    def wait(self, client_id):
+        data = "\x00\x00\x00\x00%s" % client_id
+        yield self._put_request(OP_WAIT, data)
         doclen = struct.unpack('<i', (yield bytes(4)))[0]
         rawdoc = yield bytes(doclen)
         objs = []
@@ -29,13 +36,19 @@ class SubscribingClient(MongoClient):
 class Channel(object):
     """A channel that is useful for publishing and retrieving events."""
     def __init__(self):
-        self.subscriptions = collections.defaultdict(list)
+        self.subscriptions = {}
 
     def get(self, subscriber):
         """Remove and return any pending events for subscriber."""
         waiting = self.subscriptions[subscriber][:]
         self.subscriptions[subscriber][:] = []
         return "".join(waiting)
+
+    def subscribe(self, subscriber):
+        self.subscriptions[subscriber] = []
+
+    def unsubscribe(self, subscriber):
+        del self.subscriptions[subscriber]
 
     def update(self, event):
         """Publish a new event for each subscriber on the channel."""
@@ -54,23 +67,37 @@ class SubscriptionProxy(MongoProxy):
      immediately the next time they return.
     """
     channels = collections.defaultdict(Channel)
+    subscribers = collections.defaultdict(set)
 
     def handle_request(self, info, body):
         length, id, to, opcode = info
-        if opcode in [OP_SUBSCRIBE, Ops.OP_UPDATE]:
+        if opcode in PROXIED_OPS:
             trimmed = body[4:]
-            col, rest = trimmed.split('\0', 1)
-            if opcode == OP_SUBSCRIBE:
+            try:
+                col, rest = trimmed.split('\0', 1)
+            except ValueError:
+                col = trimmed.strip('\0')
+                rest = ''
+            if opcode == OP_WAIT:
+                subscriber = col
+                chans = self.subscribers[subscriber]
+                ready = [self.channels[c].get(subscriber) for c in chans]
+                if not any(ready):
+                    chanupdates = tuple(('update',) + chan for chan in chans)
+                    yield tuple(wait(updates) for updates in chanupdates)
+                    ready = [self.channels[c].get(subscriber) for c in chans]
+                all_ready = "".join(ready)
+                rlen = len(all_ready)
+                resp = "%s%s" % (struct.pack('<i', rlen), all_ready)
+                yield up((resp, info, body))
+            elif opcode == OP_SUBSCRIBE:
                 subscriber, col = col.split('@')
                 sl, raw_bson = rest[:8], rest[8:]
                 spec = BSON(raw_bson).to_dict()
-                waiting = self.channels[(col, str(spec))].get(subscriber)
-                if not waiting:
-                    yield wait(('update', col, str(spec)))
-                    waiting = self.channels[(col, str(spec))].get(subscriber)
-                wlen = len(waiting)
-                resp = "%s%s" % (struct.pack('<i', wlen), waiting)
-                yield up((resp, info, body))
+                chan = (col, str(spec))
+                self.channels[chan].subscribe(subscriber)
+                self.subscribers[subscriber].add(chan)
+                yield up(('', info, body))
             elif opcode == Ops.OP_UPDATE:
                 upsert, raw_bson = rest[:4], rest[4:]
                 spec, raw_doc = _bson_to_dict(raw_bson)
@@ -100,7 +127,10 @@ if __name__ == '__main__':
         c = SubscribingClient()
         c.connect(BACKEND_HOST, FRONTEND_PORT)
         print "subscriber: subscribing ..."
-        events = yield c.subscribe('subscriber@sub.test', {'room':'general'})
+        yield c.subscribe('subscriber@sub.test', {'room':'general'})
+        yield c.subscribe('subscriber@sub.test', {'name':'allrooms'})
+        print "subscriber: waiting for events ..."
+        events = yield c.wait('subscriber')
         print "subscriber: saw events %r" % events
         with (yield c.sub.test.find({'room':'general'})) as cursor:
             result = yield cursor.more()
@@ -108,13 +138,15 @@ if __name__ == '__main__':
         print "subscriber: sleeping for 10 ..."
         yield sleep(10)
         print "subscriber: woke up"
-        events = yield c.subscribe('subscriber@sub.test', {'room':'general'})
+        events = yield c.wait('subscriber')
         assert len(events) == 2
         print "subscriber: there were %d events while i was sleeping" % len(events)
         print "subscriber: events: %r" % events
         with (yield c.sub.test.find({'room':'general'})) as cursor:
             result = yield cursor.more()
             print "subscriber: final room state", result
+        events = yield c.wait('subscriber')
+        print "subscriber: more events: %r" % events
         a.halt()
 
     def publisher():
@@ -131,6 +163,8 @@ if __name__ == '__main__':
         yield sleep(2)
         yield c.sub.test.update({'room':'general'}, {'$pull': {'users':'publisher'}}, upsert=1)
         print "publisher: updated the doc"
+        yield sleep(8)
+        yield c.sub.test.update({'name':'allrooms'}, {'name':'allrooms', 'value':['foo', 'bar', 'baz']}, upsert=1)
 
     a = Application()
     a.add_service(Service(SubscriptionProxy(BACKEND_HOST, BACKEND_PORT), FRONTEND_PORT))
