2301b76aef04754284ec7824db10069640e0ae0c
Christian Wyglendowski <dowski@boomplex.com>
2009-10-18 22:26:39 -0400

    Changes to the santa example to make the output more in line with what other implementation do.

--- examples/santa.py
+++ examples/santa.py
@@ -9,60 +9,53 @@ def santa():
         deer_ready, elves_ready = len(deer_group) == 9, len(elf_group) == 3
         if (deer_ready or elves_ready):
             print "santa woke up"
-        if deer_ready:
-            yield work_with_group(deer_group, 'delivers toys - HO HO HO!', 9)
-            yield excuse_group('deer', deer_group)
-        if elves_ready:
-            yield work_with_group(elf_group, 'consults with elves', 3)
-            yield excuse_group('elf', elf_group)
+            if deer_ready:
+                yield work_with_group('deer', deer_group, 'deliver toys', 9)
+                yield excuse_group('deer', deer_group)
+            if elves_ready:
+                yield work_with_group('elf', elf_group, 'meet in my study', 3)
+                yield excuse_group('elf', elf_group)
         print "santa sleeping"
         yield sleep(random.random() * 1)
 
-def elf(id):
-    def _elf():
+def actor(name, type, group, task, max_group, max_sleep):
+    def _actor():
         while True:
-            if len(elf_group) < 3:
-                elf_group.append("Elf %d" % id)
-                yield wait('elf-group-done')
-            yield sleep(random.random() * 3)
-    return _elf
+            if len(group) < max_group:
+                group.append(name)
+                yield wait('%s-group-started' % type)
+                print "%s %s" % (name, task)
+                yield wait('%s-group-done' % type)
+            yield sleep(random.random() * max_sleep)
+    return _actor
 
-def reindeer(name):
-    def _reindeer():
-        while True:
-            if len(deer_group) < 9:
-                deer_group.append(name)
-                yield wait('deer-group-done')
-            yield sleep(random.random() * 9)
-    return _reindeer
-
-def start_santa():
-    app.add_loop(Loop(santa))
-
-def start_the_elves():
-    for i in xrange(10):
-        app.add_loop(Loop(elf(i)))
-
-def start_the_reindeer():
-    for name in [
-            'Dasher', 'Dancer', 'Prancer', 
-            'Vixen', 'Comet', 'Cupid', 
-            'Donner', 'Blitzen', 'Rudolph',
-            ]:
-        app.add_loop(Loop(reindeer(name)))
-
-def work_with_group(group, message, req_length):
-    assert len(group) == req_length
-    print "santa", message
+def work_with_group(name, group, message, req_length):
+    yield fire('%s-group-started' % name)
+    print "Ho! Ho! Ho! Let's", message
     yield sleep(random.random() * 3)
-    assert len(group) == req_length
 
 def excuse_group(name, group):
     group[:] = []
     yield fire('%s-group-done' % name, True)
 
 def main():
-    global app
+    def start_santa():
+        app.add_loop(Loop(santa))
+
+    def start_the_elves():
+        task = "meets in study"
+        for i in xrange(10):
+            app.add_loop(Loop(actor("Elf %d" % i, 'elf', elf_group, task, 3, 3)))
+
+    def start_the_reindeer():
+        task = "delivers toys"
+        for name in [
+                'Dasher', 'Dancer', 'Prancer', 
+                'Vixen', 'Comet', 'Cupid', 
+                'Donner', 'Blitzen', 'Rudolph',
+                ]:
+            app.add_loop(Loop(actor(name, 'deer', deer_group, task, 9, 9)))
+
     app = Application()
     start_santa()
     start_the_elves()
