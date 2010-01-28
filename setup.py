e219a5f4a7f5b8d5cfb646d06399da98632e49e7
Jamie Turner <jamie@jamwt.com>
2010-01-28 02:12:18 -0600

    merge

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 VERSION = "0.9.2b"
 
 setup(name="diesel",
-	version=VERSION,
-	author="Boomplex LLC/Bump Technologies, Inc.",
-	author_email="dev@boomplex.com",
-	description="Diesel is a generator-based asynchronous I/O library for Python",
-	long_description='''
+    version=VERSION,
+    author="Boomplex LLC/Bump Technologies, Inc.",
+    author_email="dev@boomplex.com",
+    description="Diesel is a generator-based asynchronous I/O library for Python",
+    long_description='''
 diesel is a framework for writing network applications using asynchronous 
 I/O in Python.
 
@@ -17,7 +17,8 @@ and continuations. It performs well and handles high concurrency with ease.
 An HTTP/1.1 implementation is included as an example, which can be used 
 for building web applications.
 ''',
-	url="http://dieselweb.org",
-	download_url="http://download.dieselweb.org/diesel-%s.tar.gz" % VERSION, 
-	packages=["diesel", "diesel.protocols"],
-	)
+    url="http://dieselweb.org",
+    download_url="http://download.dieselweb.org/diesel-%s.tar.gz" % VERSION, 
+    packages=["diesel", "diesel.protocols"],
+    scripts=["examples/dhttpd"],
+    )
