2ad6d0ed2ceba5cc51d612d41a71fb04493076bd
Michael Schurter <michael@susens-schurter.com>
2010-02-21 13:20:32 -0500

    Add support for readonly UNIX pipes like stdin

--- diesel/__init__.py
+++ diesel/__init__.py
@@ -4,6 +4,7 @@ log = logmod.log
 from logmod import Logger, LOGLVL_DEBUG, LOGLVL_INFO, LOGLVL_WARN, LOGLVL_ERR, LOGLVL_CRITICAL
 from core import until, until_eol, bytes, sleep, up, Connection, ConnectionClosed, Loop
 from core import fire, wait, catch, ClientConnectionError, ClientConnectionClosed
-from app import Application, Service 
+from app import Application, Service
+from pipe import Pipe
 from client import Client, call, message, response
 from security import TLSv1ServiceWrapper, TLSv1ClientWrapper
