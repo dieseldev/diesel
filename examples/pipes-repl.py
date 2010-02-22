6e293043f85f438571f4fde1926cabfcae6e0fd7
Michael Schurter <michael@susens-schurter.com>
2010-02-21 20:06:51 -0700

    This option uses exec which makes blocks work but breaks other stuff
    Exceptions are propagated now, so this verison is probably worse than the previous version.

--- examples/pipes-repl.py
+++ examples/pipes-repl.py
@@ -10,23 +10,29 @@ def readcb():
     print 'Diesel Console'
     cmd = ''
     prompt = DEFAULT_PROMPT
-    interp = code.InteractiveInterpreter(locals={'app':current_app})
     while 1:
         sys.stdout.write(prompt)
         sys.stdout.flush()
         input = yield until("\n")
         cmd += input
         if input.lstrip() == input or input == "\n":
-            ret = code.compile_command(input)
-            if ret:
-                out = interp.runcode(ret)
-                if out:
-                    print 'Out: %r' % out
+            try:
+                ret = code.compile_command(input)
+            except SyntaxError, e:
+                # TODO Pretty print traceback
+                print e
+                # Reset repl
                 cmd = ''
                 prompt = DEFAULT_PROMPT
             else:
-                # Start of a block
-                prompt = '... '
+                if ret:
+                    #interp.runcode(ret)
+                    exec cmd
+                    cmd = ''
+                    prompt = DEFAULT_PROMPT
+                else:
+                    # Start of a block
+                    prompt = '... '
         else:
             # Continued block
             prompt = '... '
