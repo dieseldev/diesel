85768d3963798ce3417040603e9ddea197758418
Jamie Turner <jamie@jamwt.com>
2010-04-10 21:18:32 -0500

    remove timeout for now

--- diesel/util/queue.py
+++ diesel/util/queue.py
@@ -1,9 +1,10 @@
 from uuid import uuid4
 from collections import deque
 
-from diesel import wait, fire, up
+from diesel import wait, fire, up, sleep
 
 class QueueEmpty(Exception): pass
+class QueueTimeout(Exception): pass
 
 class Queue(object):
     def __init__(self):
@@ -14,11 +15,16 @@ class Queue(object):
         self.inp.append(i)
         yield fire(self.wait_id)
 
-    def get(self, waiting=True):
+    def get(self, waiting=True, timeout=None):
         if self.inp:
             yield up(self.inp.popleft())
         elif not waiting:
             raise QueueEmpty()
         else:
-            yield wait(self.wait_id)
+            if timeout:
+                v, t = yield (wait(self.wait_id), sleep(timeout))
+                if t:
+                    raise QueueTimeout()
+            else:
+                yield wait(self.wait_id)
             yield up(self.inp.popleft())
---
+++ examples/queue.py
@@ -0,0 +1,47 @@
+from diesel.util.queue import Queue, QueueTimeout
+from diesel import log as glog, Application, Loop, sleep, catch
+
+q = Queue()
+
+def putter():
+    log = glog.sublog("putter", glog.info)
+    
+    log.info("putting 50000 things on log")
+    for x in xrange(50000):
+        yield q.put(x)
+        yield sleep()
+
+    log.info("done, sleeping for 10s...")
+
+    yield sleep(10)
+
+    log.info("putting 50000 *more* things on log")
+    for x in xrange(50000, 100000):
+        yield q.put(x)
+        yield sleep()
+    a.halt()
+
+def getter():
+    log = glog.sublog("getter", glog.info)
+    got = 0
+    while got < 100000:
+        try:
+#            s = yield catch(q.get(timeout=3), QueueTimeout)
+            s = yield q.get()
+        except QueueTimeout:
+            log.warn("timeout before getting a value, retrying...")
+            continue
+        assert s == got
+        got += 1
+
+        if got % 10000 == 0:
+            log.info("up to %s received, sleeping for 0.5s" % got)
+            yield sleep(0.5)
+
+    log.info("SUCCESS!  got all 100,000")
+    a.halt()
+
+a = Application()
+a.add_loop(Loop(putter))
+a.add_loop(Loop(getter))
+a.run()
