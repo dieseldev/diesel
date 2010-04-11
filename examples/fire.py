de0da7d8c7a4d8cc9414252cb71da4c1937d26b6
Jamie Turner <jamie@jamwt.com>
2010-04-10 20:57:42 -0500

    More detail on firing speed.

--- examples/fire.py
+++ examples/fire.py
@@ -1,6 +1,7 @@
 # vim:ts=4:sw=4:expandtab
 '''Example of event firing.
 '''
+import time
 import random
 from diesel import Application, Loop, sleep, fire, wait, log
 
@@ -12,11 +13,14 @@ def gunner():
         yield sleep()
 
 def sieged():
+    t = time.time()
     while True:
         n = yield wait('bam')
         if n % 10000 == 0:
             log.info(n)
             if n == 50000:
+                delt = time.time() - t
+                log.info("50,000 messages in %.3fs (%.1f/s)" % (delt, 50000 / delt))
                 a.halt()
 
 a = Application()
