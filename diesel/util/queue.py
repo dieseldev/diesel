b318ccbfa331f08365611c805c4415dd0ee32d62
Will Moss <wbmoss@gmail.com>
2010-06-14 16:56:51 -0700

    Fixing a bug in the diesel Queue where multiple waiters on an empty queue could pop from and empty deque

--- diesel/util/queue.py
+++ diesel/util/queue.py
@@ -1,3 +1,4 @@
+from time import time
 from uuid import uuid4
 from collections import deque
 
@@ -10,24 +11,28 @@ class Queue(object):
     def __init__(self):
         self.wait_id = uuid4()
         self.inp = deque()
-    
+
     def put(self, i=None):
         self.inp.append(i)
         yield fire(self.wait_id)
 
     def get(self, waiting=True, timeout=None):
+        start = time()
+        while not self.inp and waiting:
+            if timeout:
+                remaining = timeout - (time() - start)
+                if remaining <= 0:
+                    raise QueueTimeout()
+                else:
+                    yield (wait(self.wait_id), sleep(remaining))
+            else:
+                yield wait(self.wait_id)
+
         if self.inp:
             yield up(self.inp.popleft())
         elif not waiting:
             raise QueueEmpty()
-        else:
-            if timeout:
-                v, t = yield (wait(self.wait_id), sleep(timeout))
-                if t:
-                    raise QueueTimeout()
-            else:
-                yield wait(self.wait_id)
-            yield up(self.inp.popleft())
+
 
     @property
     def is_empty(self):
