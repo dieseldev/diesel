9c6695986102b74cd0cf0e3cbc2c1084394d7221
Jamie Turner <jamie@bu.mp>
2010-06-23 23:13:10 -0700

    More examples ported to diesel2.

--- examples/queue.py
+++ examples/queue.py
@@ -1,5 +1,5 @@
 from diesel.util.queue import Queue, QueueTimeout
-from diesel import log as glog, Application, Loop, sleep, catch
+from diesel import log as glog, Application, Loop, sleep
 
 q = Queue()
 
@@ -8,25 +8,24 @@ def putter():
     
     log.info("putting 50000 things on log")
     for x in xrange(50000):
-        yield q.put(x)
-        yield sleep()
+        q.put(x)
+        sleep()
 
     log.info("done, sleeping for 10s...")
 
-    yield sleep(10)
+    sleep(10)
 
     log.info("putting 50000 *more* things on log")
     for x in xrange(50000, 100000):
-        yield q.put(x)
-        yield sleep()
-    a.halt()
+        q.put(x)
+        sleep()
 
 def getter():
     log = glog.sublog("getter", glog.info)
     got = 0
     while got < 100000:
         try:
-            s = yield catch(q.get(timeout=3), QueueTimeout)
+            s = q.get(timeout=3)
         except QueueTimeout:
             log.warn("timeout before getting a value, retrying...")
             continue
@@ -35,7 +34,7 @@ def getter():
 
         if got % 10000 == 0:
             log.info("up to %s received, sleeping for 0.5s" % got)
-            yield sleep(0.5)
+            sleep(0.5)
 
     log.info("SUCCESS!  got all 100,000")
     a.halt()
