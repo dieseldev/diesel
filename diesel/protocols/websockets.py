a0c0fccd4ab948f57be710f7a6d6ebfcdcbaac18
Jamie Turner <jamie@bu.mp>
2010-08-30 13:21:05 -0700

    Adding initial support for WebSockets.

---
+++ diesel/protocols/websockets.py
@@ -0,0 +1,70 @@
+from .http import HttpServer, HttpHeaders
+from diesel.util.queue import Queue
+from diesel import fork, until, receive, first, ConnectionClosed, send
+from simplejson import dumps, loads
+
+class WebSocketDisconnect(object): pass
+class WebSocketData(dict): pass
+
+class WebSocketServer(HttpServer):
+    '''Very simple Web Socket server.
+    '''
+    def __init__(self, web_handler, web_socket_handler, ws_location):
+        self.web_handler = web_handler
+        self.web_socket_handler = web_socket_handler
+        self.ws_location = ws_location
+        HttpServer.__init__(self, self.do_upgrade)
+
+    def do_upgrade(self, req):
+        if req.headers.get_one('Upgrade') != 'WebSocket':
+            return self.web_handler(req)
+
+        # do upgrade response
+        org = req.headers.get_one('Origin')
+
+        send(
+'''HTTP/1.1 101 Web Socket Protocol Handshake\r
+Upgrade: WebSocket\r
+Connection: Upgrade\r
+WebSocket-Origin: %s\r
+WebSocket-Location: %s\r
+WebSocket-Protocol: diesel-generic\r
+\r
+''' % (org, self.ws_location))
+        
+        inq = Queue()
+        outq = Queue()
+
+        def wrap(inq, outq):
+            self.web_socket_handler(inq, outq)
+            outq.put(WebSocketDisconnect())
+
+        fork(wrap, inq, outq)
+                                    
+        while True:
+            try:
+                typ, val = first(receive=1, waits=[outq.wait_id])
+                if typ == 'receive':
+                    assert val == '\x00'
+                    val = until('\xff')[:-1]
+                    if val == '':
+                        inq.put(WebSocketDisconnect())
+                    else:
+                        data = loads(val)
+                        inq.put(WebSocketData(data))
+                else:
+                    try:
+                        v = outq.get(waiting=False)
+                    except QueueEmpty:
+                        pass
+                    else:
+                        if type(v) is WebSocketDisconnect:
+                            send('\x00\xff')
+                            break
+                        else:
+                            data = dumps(dict(v))
+                            send('\x00%s\xff' % data)
+
+            except ConnectionClosed:
+                inq.put(WebSocketDisconnect)
+                raise ConnectionClosed("remote disconnected")
---
+++ examples/ws.py
@@ -0,0 +1,67 @@
+from diesel.protocols.http import http_response, HttpHeaders
+from diesel.protocols.websockets import WebSocketServer, WebSocketData as WSD
+from diesel import Service, Application, sleep
+from diesel.util.queue import QueueTimeout
+
+LOCATION = "ws://172.16.26.129:8091/"
+
+content = '''
+<html>
+<head>
+<script>
+
+var chatter = new WebSocket("%s");
+
+chatter.onopen = function (evt) {
+}
+
+chatter.onmessage = function (evt) {
+    var p = document.getElementById("the-p");    
+    p.innerHTML = evt.data;
+}
+
+function push () {
+    var i = document.getElementById("the-i");
+    chatter.send('{"message" : "' + i.value + '"}');
+}
+
+</script>
+
+</head>
+<body>
+
+<h1>Hello dude!</h1>
+
+<p id="the-p">
+</p>
+
+<input type="text" size="40" id="the-i" /> <input type="button" value="Update Message" onclick="push(); return false" />
+
+</body>
+</html>
+''' % LOCATION
+
+def web_handler(req):
+    heads = HttpHeaders()
+    heads.add('Content-Length', len(content))
+    heads.add('Content-Type', 'text/html')
+
+    return http_response(req, 200, heads, content)
+
+import time
+
+def socket_handler(inq, outq):
+    message = "hello, there!"
+    while True:
+        try:
+            v = inq.get(timeout=0.5)
+        except QueueTimeout:
+            pass
+        else:
+            message = v['message']
+                        
+        outq.put(WSD(message=message, time=time.time()))
+
+a = Application()
+a.add_service(Service(WebSocketServer(web_handler, socket_handler, LOCATION), 8091))
+a.run()
