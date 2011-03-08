0f8c1484c07f65bef73cb9da923374bbcedbd763
Jamie Turner <jamie@jamwt.com>
2011-03-08 14:27:05 -0600

    Updated for 2.X releases.

--- setup.py
+++ setup.py
@@ -12,21 +12,22 @@ VERSION = "1.9.7b"
 
 setup(name="diesel",
     version=VERSION,
-    author="Boomplex LLC/Bump Technologies, Inc.",
-    author_email="dev@boomplex.com",
-    description="Diesel is a generator-based asynchronous I/O library for Python",
+    author="Jamie Turner/Boomplex LLC/Bump Technologies, Inc/Various Contributors",
+    author_email="jamie@bu.mp",
+    description="Diesel is a coroutine-based asynchronous I/O library for Python",
     long_description='''
 diesel is a framework for writing network applications using asynchronous 
 I/O in Python.
 
-It uses Python's generators to provide a friendly syntax for coroutines 
+It uses the greenlet library to provide a friendly syntax for coroutines 
 and continuations. It performs well and handles high concurrency with ease.
 
-An HTTP/1.1 implementation is included as an example, which can be used 
-for building web applications.
+An HTTP/1.1+WSGI implementation is included as an example, which can be used 
+for building web applications.  Other bundled protocols include MongoDB, 
+Redis, Beanstalkd, and PostgreSQL.
 ''',
     url="http://dieselweb.org",
-    download_url="http://download.dieselweb.org/diesel-%s.tar.gz" % VERSION, 
+    download_url="http://jamwt.com/diesel/diesel-%s.tar.gz" % VERSION, 
     packages=["diesel", "diesel.protocols", "diesel.util"],
     scripts=["examples/dhttpd"],
     install_requires=(["greenlet", "pyopenssl"] + additional_requires),
