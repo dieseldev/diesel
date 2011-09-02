e923ebdad7eef4036ca8a421f2601776552215c0
Jamie Turner <jamie@bu.mp>
2011-09-02 01:56:47 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import sys 
-assert sys.version_info >= (2, 5), \
-"Diesel requires python 2.5 (or greater 2.X release)"
+assert sys.version_info >= (2, 6), \
+"Diesel requires python 2.6 (or greater 2.X release)"
 
 from setuptools import setup
 
@@ -12,20 +12,26 @@ setup(name="diesel",
     version=VERSION,
     author="Jamie Turner/Boomplex LLC/Bump Technologies, Inc/Various Contributors",
     author_email="jamie@bu.mp",
-    description="Diesel is a coroutine-based asynchronous I/O library for Python",
+    description="Diesel is a coroutine-based networking library for Python",
     long_description='''
-diesel is a framework for writing network applications using asynchronous 
-I/O in Python.
+diesel is a framework for easily writing reliable and scalable network
+applications in Python.  It uses the greenlet library layered atop
+asynchronous socket I/O in Python to achieve benefits of both
+the threaded-style (linear, blocking-ish code flow) and evented-style
+(no locking, low overhead per connection) concurrency paradigms.  It's
+design is heavily inspired by the Erlang/OTP platform.
 
-It uses the greenlet library to provide a friendly syntax for coroutines 
-and continuations. It performs well and handles high concurrency with ease.
+It contains high-quality buffering, queuing and synchronization primitives,
+procedure supervision and supervision trees, connection pools, seamless
+thread integration, and more.
 
-An HTTP/1.1+WSGI implementation is included as an example, which can be used 
-for building web applications.  Other bundled protocols include MongoDB, 
-Redis, Beanstalkd, and PostgreSQL.
+An HTTP/1.1+WSGI+WebSockets implementation is included, as well as tight
+integration with the Flask web framework.
+
+Other bundled protocols include MongoDB and Redis client libraries.
 ''',
     url="http://dieselweb.org",
-    download_url="http://jamwt.com/diesel/diesel-%s.tar.gz" % VERSION, 
+    download_url="http://jamwt.com/diesel/diesel-%s.tar.gz" % VERSION,
     packages=["diesel", "diesel.protocols", "diesel.util"],
     scripts=["examples/dhttpd"],
     entry_points={
