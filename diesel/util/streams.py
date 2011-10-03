72eca2a540c5d2fc197490f3cc61e8485b909b89
Jamie Turner <jamie@bu.mp>
2011-10-03 12:17:53 -0700

    Change for EOF behavior.

--- diesel/util/streams.py
+++ diesel/util/streams.py
@@ -2,8 +2,6 @@ import thread
 from diesel.util.queue import Queue
 from diesel import fork_from_thread
 
-class StreamEnd(object): pass
-
 def put_stream_token(q, line):
     q.put(line)
 
@@ -11,6 +9,8 @@ def consume_stream(stream, q):
     while True:
         line = stream.readline()
         fork_from_thread(put_stream_token, q, line)
+        if line == '':
+            break
 
 def create_line_input_stream(fileobj):
     q = Queue()
