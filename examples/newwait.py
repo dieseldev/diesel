4cb160edcd979c3197b65d7207b3ee9c86d14a78
Jamie Turner <jamie@jamwt.com>
2011-10-04 20:39:06 -0500

    Example with new wait-on-queues.

---
+++ examples/newwait.py
@@ -0,0 +1,28 @@
+import random
+
+from diesel import quickstart, first, sleep, fork
+from diesel.util.queue import Queue
+
+def fire_random(queues):
+    while True:
+        sleep(1)
+        random.choice(queues).put(None)
+
+def make_and_wait():
+    q1 = Queue()
+    q2 = Queue()
+    both = [q1, q2]
+
+    fork(fire_random, both)
+
+    while True:
+        q, v = first(waits=both)
+        assert v is None
+        if q == q1:
+            print 'q1'
+        elif q == q2:
+            print 'q2'
+        else:
+            assert 0
+
+quickstart(make_and_wait)
