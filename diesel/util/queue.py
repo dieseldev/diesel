cefcd5cdc572deac296d5eb994dbc7ff8f28316c
Jamie Turner <jamie@bu.mp>
2012-03-01 23:41:24 -0800

    Fanout object (multi-sub with queue)

--- diesel/util/queue.py
+++ diesel/util/queue.py
@@ -1,6 +1,7 @@
 from time import time
 from uuid import uuid4
 from collections import deque
+from contextlib import contextmanager
 
 from diesel import wait, fire, sleep, first
 from diesel.events import Waiter, StopWaitDispatch
@@ -51,3 +52,20 @@ class Queue(Waiter):
 
     def ready_early(self):
         return not self.is_empty
+
+class Fanout(object):
+    def __init__(self):
+        self.subs = set()
+
+    def pub(self, m):
+        for s in self.subs:
+            s.put(m)
+
+    @contextmanager
+    def sub(self):
+        q = Queue()
+        self.subs.add(q)
+        try:
+            yield q
+        finally:
+            self.subs.remove(q)
---
+++ examples/fanout.py
@@ -0,0 +1,30 @@
+from diesel import quickstart, quickstop, sleep
+from diesel.util.queue import Fanout
+from diesel.util.event import Countdown
+
+LISTENERS = 10
+EVENTS = 5
+
+cd = Countdown(LISTENERS * EVENTS)
+
+f = Fanout()
+
+def listener(x):
+    with f.sub() as q:
+        while True:
+            v = q.get()
+            print '%s <- %s' % (x, v)
+            cd.tick()
+
+def teller():
+    for x in xrange(EVENTS):
+        sleep(2)
+        f.pub(x)
+
+def killer():
+    cd.wait()
+    quickstop()
+
+from functools import partial
+quickstart(killer, teller,
+        *[partial(listener, x) for x in xrange(LISTENERS)])
