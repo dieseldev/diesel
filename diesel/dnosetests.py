b3b9879571ff6b2cf08741924d45aa945e689204
Christian Wyglendowski <christian@bu.mp>
2012-03-06 21:03:48 -0500

    s/loglevels/levels in dnosetests.

--- diesel/dnosetests.py
+++ diesel/dnosetests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Run nosetests in the diesel event loop.
 
-You can pass the same command-line arguments that you can pass to the 
+You can pass the same command-line arguments that you can pass to the
 `nosetests` command to this script and it will execute the tests in the
 diesel event loop. This is a great way to test interactions between various
 diesel green threads and network-based applications built with diesel.
@@ -10,11 +10,11 @@ diesel green threads and network-based applications built with diesel.
 import diesel
 import nose
 
-from diesel.logmod import loglevels, set_log_level
+from diesel.logmod import levels, set_log_level
 
 def main():
-    set_log_level(loglevels.ERROR)
-    quickstart(nose.main)
+    set_log_level(levels.ERROR)
+    diesel.quickstart(nose.main)
 
 if __name__ == '__main__':
     main()
