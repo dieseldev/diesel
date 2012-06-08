3ee36acfb0e51a3b6c9391ecebcd3651762b0483
Jamie Turner <jamie@bu.mp>
2012-06-08 01:23:26 -0700

    Dreadlock client.

---
+++ examples/dreadlocks.py
@@ -0,0 +1,16 @@
+'''Test for client to dreadlock network lock service.
+'''
+
+import uuid
+from diesel import sleep, quickstart
+from diesel.protocols.dreadlock import DreadlockService
+
+locker = DreadlockService('localhost', 6001)
+def f():
+    with locker.hold("foo", 30):
+        id = uuid.uuid4()
+        print "start!", id
+        sleep(2)
+        print "end!", id
+
+quickstart(f, f, f, f, f)
