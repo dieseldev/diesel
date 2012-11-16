ff7ae3eb28326595786a157c8c85c388c18fce0d
Christian Wyglendowski <christian@bu.mp>
2012-11-16 10:00:35 -0500

    Logging fix so that .fields() logging works.
    Previously those values were discarded from the log output.

--- diesel/logmod.py
+++ diesel/logmod.py
@@ -16,7 +16,7 @@ diesel_format.conversion.add("name", str, "{{{1}}}".format)
 diesel_format.conversion.add("level", str, "{1}".format)
 diesel_format.conversion.aggregate = " ".join
 diesel_format.conversion.genericValue = str
-diesel_format.conversion.genericItem = lambda _1, _2: ""
+diesel_format.conversion.genericItem = lambda _1, _2: "%s=%s" % (_1, _2)
 
 diesel_output = outputs.StreamOutput(diesel_format)
 
