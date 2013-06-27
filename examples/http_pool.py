571577e736b94447549a01ffc8d5eb5fe472ec36
Jamie Turner <jamie@bu.mp>
2013-06-27 01:13:14 -0700

    Print timings.

--- examples/http_pool.py
+++ examples/http_pool.py
@@ -1,9 +1,23 @@
+import sys
+from time import time
+
 from diesel import quickstart, quickstop
 from diesel.protocols.http.pool import request
 
 def f():
-    print request("http://example.iana.org/"), 'is found?'
+    t1 = time()
     print request("http://example.iana.org/missing"), 'is missing?'
+    t2 = time()
+    print request("http://example.iana.org/missing"), 'is missing?'
+    t3 = time()
+    print request("http://example.iana.org/missing"), 'is missing?'
+    t4 = time()
+    print request("http://example.iana.org/"), 'is found?'
+    t5 = time()
+
+    print 'First request should (probably) have been longer (tcp handshake) than subsequent 3 requests:'
+    reduce(lambda t1, t2: sys.stdout.write("%.4f\n" % (t2 - t1)) or t2, (t1, t2, t3, t4, t5))
+
     quickstop()
 
 quickstart(f)
