3259356f982b8ff27030f29ac89e4b7eec50d07e
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2015-03-14 20:19:42 +0100

    Fix clocker example

--- examples/clocker.py
+++ examples/clocker.py
@@ -1,6 +1,9 @@
-import os
-from diesel import Loop, fork, Application, sleep
+from diesel import Loop, fork, Application, sleep, quickstop, fire, wait
 from diesel.util.stats import CPUStats
+from diesel.util.queue import Queue
+
+WORKERS = 3
+WORKER_QUEUE = Queue()
 
 def not_always_busy_worker():
     with CPUStats() as stats:
@@ -8,11 +11,17 @@ def not_always_busy_worker():
             for i in range(10000000): # do some work to forward cpu seconds
                 pass
             sleep(0.1) # give up control
-
     print("cpu seconds ",  stats.cpu_seconds)
+    WORKER_QUEUE.put(None)
+
+def teardown():
+    for _ in range(WORKERS):
+        wait(WORKER_QUEUE)
+    quickstop()
 
 def spawn_busy_workers():
-    for _ in range(0,3):
+    fork(teardown)
+    for _ in range(WORKERS):
         fork(not_always_busy_worker)
 
 a = Application()
